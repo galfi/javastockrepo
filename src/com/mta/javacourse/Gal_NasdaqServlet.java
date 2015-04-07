324b7f9ad895fca8684b327609d49823f8f0e1ec
'galfi' <galf2110@gmail.com>
2015-04-07 11:47:49 +0300

    Ex03- Advanced Math version2

--- src/com/mta/javacourse/Gal_NasdaqServlet.java
+++ src/com/mta/javacourse/Gal_NasdaqServlet.java
@@ -21,17 +21,15 @@ public class Gal_NasdaqServlet extends HttpServlet {
 		double result1= radius * pi;
 		
 		String line1= new String ("<h1> Calculation 1: Area of circle with radius: "+radius+" * "+pi+" = "+result1+" </h1>");	
-		resp.getWriter().println(line1);
 		
 		int opposite;
 		int hypotenuse;
 			
 		opposite=30;
 		hypotenuse=50;
-		double result2= opposite / hypotenuse ;
+		float result2= opposite / hypotenuse ;
 		
 		String line2= new String ("<h1> Calculation 2: Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is: "+opposite+" / "+hypotenuse+" = "+result2+"  cm </h1>");	
-		resp.getWriter().println(line2);
 		
 		int base;
 		int exp;
@@ -41,7 +39,6 @@ public class Gal_NasdaqServlet extends HttpServlet {
 		double result3= base ^ exp;
 		
 		String line3= new String ("<h1> Calculation 3: Power of 20 with exp of 13 is: "+base+" ^ "+exp+" = "+result3+"  cm </h1>");	
-		resp.getWriter().println(line3);
 		
 		String resultStr= line1 + "<br>" + line2 + "<br>" +line3;
 		resp.getWriter().println (resultStr);
